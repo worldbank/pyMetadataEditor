1fbe01e53abd385d1cf8e86e64f6b008b47c6fe7
Gordon Blackadder <iblackadder@worldbank.org>
2024-06-12 09:47:31 -0400

    run precommit

--- pymetadataeditor/interface.py
+++ pymetadataeditor/interface.py
@@ -1,13 +1,13 @@
+from typing import Dict, Union
+
+import pandas as pd
 import requests
 from requests.exceptions import HTTPError
-import pandas as pd
-from typing import Dict, Union, Optional
 
-class MetadataEditor():
 
+class MetadataEditor:
     def __init__(self, api_key: str):
-        """
-        """
+        """ """
         self.api_key = api_key
 
     def _request(self, method: str, url: str, **kwargs):
@@ -26,13 +26,13 @@ class MetadataEditor():
             Exception: If the request fails due to other HTTP errors, with details of the status code and response text.
             Exception: If any other unexpected error occurs during the request.
         """
-        assert method in ['get', 'post'], f"unknown method {method}"
-        if method == 'post':
-            assert 'json' in kwargs, "when using post, json cannot be none"
-        
+        assert method in ["get", "post"], f"unknown method {method}"
+        if method == "post":
+            assert "json" in kwargs, "when using post, json cannot be none"
+
         if not url.startswith("https://"):
             raise ValueError(f"URL must start with 'https://' not {url[:8]}")
-        
+
         try:
             response = requests.request(method, url, headers={"x-api-key": self.api_key}, **kwargs)
             response.raise_for_status()
@@ -40,43 +40,40 @@ class MetadataEditor():
             if response.status_code == 403:
                 raise PermissionError(f"Access Denied. Check that the API key '{self.api_key}' is correct")
             else:
-                raise Exception(f"Error: Failed to get collections. Status code: {response.status_code} Response: {response.text}")
+                raise Exception(
+                    f"Error: Failed to get collections. Status code: {response.status_code} Response: {response.text}"
+                ) from e
         except Exception as e:
             raise Exception(f"An unexpected error occurred: {str(e)}") from e
         return response.json()
 
     def _get_request(self, url: str) -> Dict[str, str]:
-        return self._request('get', url=url)
-    
+        return self._request("get", url=url)
+
     def _post_request(self, url: str, metadata: Dict[str, Union[str, Dict]]):
-        self._request('post', url=url, json=metadata)
+        self._request("post", url=url, json=metadata)
 
     def list_collections(self) -> pd.DataFrame:
-        """
-        """
+        """ """
         list_collections_get_url = "https://metadataeditorqa.worldbank.org/index.php/api/editor"
         response = self._get_request(list_collections_get_url)
-        project_collection = response['projects']
+        project_collection = response["projects"]
 
-        return pd.DataFrame.from_dict(project_collection).set_index('id').sort_values("created")
+        return pd.DataFrame.from_dict(project_collection).set_index("id").sort_values("created")
 
     def _quick_validate_metadata(self, metadata: Dict[str, Union[str, Dict]]):
-        """
-        """
-        if type(metadata) != dict:
+        """ """
+        if not isinstance(metadata, dict):
             raise ValueError(f"Metadata must be passed as a python dict, but {type(metadata)} was passed instead")
         required_keys = ["idno"]  # todo(gblackadder) what are the fields all datatypes must have
-    
+
         for key in required_keys:
             if key not in metadata:
                 raise ValueError(f"Metadata is missing required key: '{key}'")
 
-
     def create_timeseries(self, metadata: Dict[str, Union[str, Dict]]):
-        """
-        """
+        """ """
         self._quick_validate_metadata(metadata)
 
         post_request_url = "https://metadataeditorqa.worldbank.org/index.php/api/editor/create/timeseries"
         self._post_request(post_request_url, metadata=metadata)
-    
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,8 +1,8 @@
-from pymetadataeditor.interface import MetadataEditor
-import requests
-import pytest
 import pandas as pd
+import pytest
+import requests
 
+from pymetadataeditor.interface import MetadataEditor
 
 
 class MockResponse:
@@ -15,66 +15,78 @@ class MockResponse:
             raise requests.exceptions.HTTPError("403 Client Error: Forbidden for url", response=self)
         elif self.status_code != 200:
             raise requests.exceptions.HTTPError(f"{self.status_code} Error")
-        
+
     def json(self):
         return self.json_data
 
+
 @pytest.mark.parametrize("method", ["get", "post"])
 def test_given_request(monkeypatch, method):
-    me = MetadataEditor(api_key = 'test')
-    if method == 'get':
-        func = lambda *args, **kwargs: me._get_request(*args, **kwargs)
-    elif method == 'post':
-        func = lambda *args, **kwargs: me._post_request(*args, **kwargs, metadata={})
-    
+    me = MetadataEditor(api_key="test")
+    if method == "get":
+
+        def func(*args, **kwargs):
+            return me._get_request(*args, **kwargs)
+
+    elif method == "post":
+
+        def func(*args, **kwargs):
+            return me._post_request(*args, **kwargs, metadata={})
+
     # url is not https
-    url = 'http://example.com' 
+    url = "http://example.com"
     with pytest.raises(ValueError):
         func(url)
 
     # api key raises Access Denied
-    url = 'https://example.com'
+    url = "https://example.com"
+
     def mock_response(*args, **kwargs):
         return MockResponse(status_code=403)
-    monkeypatch.setattr(requests, 'request', mock_response)
+
+    monkeypatch.setattr(requests, "request", mock_response)
     with pytest.raises(PermissionError):
         func(url)
-    
+
     # api raises some other http error
     def mock_response(*args, **kwargs):
         return MockResponse(status_code=502)
-    monkeypatch.setattr(requests, 'request', mock_response)
+
+    monkeypatch.setattr(requests, "request", mock_response)
     with pytest.raises(Exception):
         func(url)
 
     # response is good
     def mock_response(*args, **kwargs):
         return MockResponse(status_code=200, json_data={})
-    monkeypatch.setattr(requests, 'request', mock_response)
+
+    monkeypatch.setattr(requests, "request", mock_response)
     func(url)
 
 
 def test_list_collections(monkeypatch):
-    collections = {'status': 'success',
-                   'projects': [{'id': '1',
-                                 'created': '2024-06-11T09:58:14-04:00'},
-                                {'id': '2',
-                                 'created': '2024-06-11T09:58:14-04:00'}
-                                 ],
-                  }
+    collections = {
+        "status": "success",
+        "projects": [
+            {"id": "1", "created": "2024-06-11T09:58:14-04:00"},
+            {"id": "2", "created": "2024-06-11T09:58:14-04:00"},
+        ],
+    }
+
     def mock_get(*args, **kwargs):
         return MockResponse(status_code=200, json_data=collections)
-    monkeypatch.setattr(requests, 'request', mock_get)
 
-    me = MetadataEditor(api_key='test')
+    monkeypatch.setattr(requests, "request", mock_get)
+
+    me = MetadataEditor(api_key="test")
     actual_collections = me.list_collections()
     assert type(actual_collections) == pd.DataFrame
     assert actual_collections.shape == (2, 1)
-    assert actual_collections.columns == ['created']
+    assert actual_collections.columns == ["created"]
+
 
 def test__quick_validate_metadata():
-    me = MetadataEditor(api_key='test')
-
+    me = MetadataEditor(api_key="test")
 
     data_is_string = """{"idno": "GB123"}"""
     with pytest.raises(ValueError):
@@ -87,16 +99,13 @@ def test__quick_validate_metadata():
 
 def test_create_timeseries(monkeypatch):
     metadata = {
-            "idno": "GB123",
-            "series_description": {
-                "idno": "string",
-                "doi": "string",
-                "name": "Gordons Test",
-                "display_name": "string"
-                }
-            }
+        "idno": "GB123",
+        "series_description": {"idno": "string", "doi": "string", "name": "Gordons Test", "display_name": "string"},
+    }
+
     def mock_post(*args, **kwargs):
         return MockResponse(status_code=200)
-    monkeypatch.setattr(requests, 'request', mock_post)
-    me = MetadataEditor(api_key='test')
+
+    monkeypatch.setattr(requests, "request", mock_post)
+    me = MetadataEditor(api_key="test")
     me.create_timeseries(metadata=metadata)
